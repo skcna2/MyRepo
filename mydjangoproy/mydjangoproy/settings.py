6f9e58cacd5da91c5b1e0fab04f6aca4af9e62c4
skcna2 <skcna2@gmail.com>
2025-01-22 17:59:37 +0100

    cambios

--- mydjangoproy/mydjangoproy/settings.py
+++ mydjangoproy/mydjangoproy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os  #IMPORTADO PARA LAS STATIC CSS y demas
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,6 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#RUTA STATIc donde están los CSS
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')       
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
