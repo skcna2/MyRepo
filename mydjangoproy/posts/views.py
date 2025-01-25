fd3863ae0d12c7165e72548b068eff2a5ce5769a
skcna2 <skcna2@gmail.com>
2025-01-25 23:17:35 +0100

    okey

--- mydjangoproy/posts/views.py
+++ mydjangoproy/posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
+    posts = Post.objects.all()
     #agregamos la template
-    return render(request, 'posts/posts_list.html')  
+    return render(request, 'posts/posts_list.html', {'posts':posts})
+  
+
